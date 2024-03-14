70088deb9152dc6a38c759a7f8054755f87eaa86
CGU <Mohameddeaf65@gmail.com>
2024-03-14 14:21:47 +0200

    101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,14 +2,13 @@
 """Type-annotated function safely_get_value
 that takes a dict and a key as
 arguments and returns the value."""
-from typing import Union, Any, TypeVar, Mapping
-
-T = TypeVar('T')
+from typing import Tuple, List
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """Return the value of the key if it exists, otherwise the default."""
-    if key in dct:
-        return dct[key]
-    else:
-        return default
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
