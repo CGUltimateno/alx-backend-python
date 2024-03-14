56ac10f4ea25d56f619a9803cac1cfd4d8662c0f
CGU <Mohameddeaf65@gmail.com>
2024-03-14 13:08:49 +0200

    0

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Type-annotated function element_length that takes a list input_list of strings as argument and returns a list."""
-from typing import List, Tuple
+from typing import Tuple, List,Iterable,Sequence
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return a list of tuples containing the string and its length."""
     return [(i, len(i)) for i in lst]
