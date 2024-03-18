96169c76909b7291b72ef7ec76ccbe673e0b735a
CGU <Mohameddeaf65@gmail.com>
2024-03-18 20:41:34 +0200

    4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,20 +5,24 @@ alter it into a new function task_wait_n.
 The code is nearly identical to wait_n
 except task_wait_random is being called.
 """
+from typing import (
+    List,
+    Callable,
+    Awaitable
+)
 import asyncio
-import typing
 
 
-async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Take the code from wait_n and
     alter it into a new function task_wait_n.
     The code is nearly identical to wait_n
     except task_wait_random is being called.
     """
-    task_wait_random: typing.Callable[[float], typing.Awaitable[float]] = \
+    task_wait_random: Callable[[float], Awaitable[float]] = \
         __import__('3-tasks').task_wait_random
-    my_lis = typing.List[float] = []
+    my_lis = List[float] = []
 
     for i in range(0, n):
         my_lis.append(await task_wait_random(max_delay))
